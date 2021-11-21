de09f96f8097041d0f71b5c6b7e3e7a959c96f68
fet64 <fet64@outlook.com>
2021-11-21 16:26:44 +0000

    random edits

--- 1-setup.sh
+++ 1-setup.sh
@@ -27,12 +27,12 @@ echo "       Setup Language to US and set locale       "
 echo "-------------------------------------------------"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-timedatectl --no-ask-password set-timezone America/Chicago
+timedatectl --no-ask-password set-timezone Europe/Stockholm
 timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="sv_SE.UTF-8"
 
 # Set keymaps
-localectl --no-ask-password set-keymap us
+localectl --no-ask-password set-keymap se
 
 # Add sudo no password rights
 sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
@@ -54,6 +54,16 @@ PKGS=(
 'xorg-drivers'
 'xorg-xkill'
 'xorg-xinit'
+'archlinux-keyring'
+'qemu'
+'virt-manager'
+'virt-viewer'
+'dnsmasq'
+'vde2'
+'bridge-utils'
+'openbsd-netcat'
+'libguestfs'
+'dmidecode'
 'xterm'
 'plasma-desktop' # KDE Load second
 'alsa-plugins' # audio plugins
